442ba2c965f2ad70c323b1bc95e8ff859439c7c2
lin-li <linli.cs@gmail.com>
2015-03-19 22:03:18 -0700

    lab2 partII: distributing mapReduce jobs

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -28,7 +28,35 @@ func (mr *MapReduce) KillWorkers() *list.List {
 	return l
 }
 
+func AssignJob(mr *MapReduce, k int, op JobType, idle chan string) {
+	var jobArgs *DoJobArgs
+	var reply DoJobReply
+	var worker string
+	switch op {
+	case Map:
+		jobArgs = &DoJobArgs{mr.file, Map, k, mr.nReduce}
+	case Reduce:
+		jobArgs = &DoJobArgs{mr.file, Reduce, k, mr.nMap}
+	}
+	select {
+	case worker = <-mr.registerChannel:
+		mr.Workers[worker] = &WorkerInfo{worker}
+	case worker = <-idle:
+	}
+	go func() {
+		call(worker, "Worker.DoJob", jobArgs, &reply)
+		idle <- worker
+	}()
+}
+
 func (mr *MapReduce) RunMaster() *list.List {
 	// Your code here
+	idle := make(chan string)
+	for i := 0; i < mr.nMap; i++ {
+		AssignJob(mr, i, Map, idle)
+	}
+	for i := 0; i < mr.nReduce; i++ {
+		AssignJob(mr, i, Reduce, idle)
+	}
 	return mr.KillWorkers()
 }
